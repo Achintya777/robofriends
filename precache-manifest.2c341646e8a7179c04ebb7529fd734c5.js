66d7e4a004b5a46f7bc3a3cb6dd4b2ec73c413be
Achintya777 <achintyaiitd7@gmail.com>
2020-07-29 16:08:04 +0530

    Updates

---
+++ precache-manifest.2c341646e8a7179c04ebb7529fd734c5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03aeba444de67e5b785a8de108c1d002",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "e9c97b84de84f0d8d725",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "ebe81a92548e1b29bbe3",
+    "url": "/robofriends/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "e9c97b84de84f0d8d725",
+    "url": "/robofriends/static/js/2.0b5b9267.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.0b5b9267.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebe81a92548e1b29bbe3",
+    "url": "/robofriends/static/js/main.0742f3d3.chunk.js"
+  },
+  {
+    "revision": "e7d2cbb368e0813da61d",
+    "url": "/robofriends/static/js/runtime-main.74eee7b5.js"
+  }
+]);
